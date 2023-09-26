0bb6fe49fb754e2cdd40fe34243e64be14c07d67
NguyenBao-git <144542482+NguyenBao-git@users.noreply.github.com>
2023-09-26 17:04:37 +0700

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//Lay du lieu GET
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/user", checkLogin, (req, res) => {
+  console.log("Here");
+  res.send("Xin chao Hoc Tran");
+  //res.send("Hello world");
+});
+
+//Login
+let users = [{ username: "hoctran", password: "123456789" }];
+app.post("/login", (req, res) => {
+  //logic cau request
+  console.log(req.body);
+  if (
+    req.body.username === users[0].username &&
+    req.body.password === users[0].password
+  ) {
+    res.status(200).json({ message: "Dang nhap thanh cong" });
+    return;
+  }
+  res.status(400).json({ message: "Dang nhap that bai" });
+});
+//Dang ky
+app.put("/resgister", (req, res) => {
+  res.status(200).json({ message: "Day la PUT Request" });
+});
+
+app.get("/",(req,res)=>{res.send("Hello bro")}) 
+//middleware Da dang nhap chua
+function checkLogin(req, res, next) {
+  let isLogin = true;
+  if (!isLogin) {
+    res.send("Chua Dang nhap");
+    return;
+  }
+  next();
+}
+
+app.listen(3000);
